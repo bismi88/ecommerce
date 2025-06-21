260b051f22e99ebb58ef7d5d92bf99e3bacd9c6a
bismijeeva selvi <bismijeeva2005@gmail.com>
2025-06-21 11:39:13 +0530

    final project

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -3,11 +3,11 @@ export default function Heading() {
     <div className="image pt-5 relative">
       <img
         className="w-full h-screen object-cover"
-        src="https://websitedemos.net/blingg-jewelry-store-04/wp-content/uploads/sites/1119/2022/08/hero.jpg"/>
+        src="https://websitedemos.net/blingg-jewelry-store-04/wp-content/uploads/sites/1119/2022/08/hero.jpg" />
       <div className="english text-left pt-5 pl-15 absolute top-1/4 left-10 font-light max-w-2xl ">
         <h5 className="new pb-8 text-sm font-light">New collection</h5>
         <h1 className="latin  text-6xl font- scheme-light font-serif">THE NEW RING <br /> </h1><br />
-         <h1 className="word text-6xl font-serif  "> SENSATION</h1>
+        <h1 className="word text-6xl font-serif  "> SENSATION</h1>
         <h4 className="words text-2xl mt-4"><br />
           “Pain itself is something to be experienced, connected to the soul.
           To be elite is to tell, bound by grief and care, the lion’s soft touch.”
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,25 @@ import {RouterProvider , createBrowserRouter} from 'react-router-dom'
 import Footer from './pages/Footer/Footer.jsx'
 import Rings from './Rings/Rings.jsx'
 import Header from './Header.jsx'
+import About from './About.jsx'
+import Contact from './Contact.jsx'
+import Bracelets from './Bracelets/Bracelets.jsx'
+import Earings from './Earings/Earings.jsx'
+import Necklaces from './Necklaces/Necklaces.jsx'
+import Productdetail from './Productdetail/Ring1.jsx'
+import ProductDetail1 from './Productdetail/Ring2.jsx'
+import ProductDetail2 from './Productdetail/Ring3.jsx'
+import ProductDetail3 from './Productdetail/Ring4.jsx'
+import ProductDetail4 from './Productdetail/Ring5.jsx'
+import ProductDetail5 from './Productdetail/Ring6.jsx'
+import ProductDetail11 from './Productdetail/Necklace2.jsx'
+import ProductDetail10 from './Productdetail/Necklace1.jsx'
+import ProductDetail9 from './Productdetail/Earing2.jsx'
+import ProductDetail8 from './Productdetail/Earing1.jsx'
+import ProductDetail7 from './Productdetail/Bracelet2.jsx'
+import ProductDetail6 from './Productdetail/Bracelet1.jsx'
+
+
 
 const router = createBrowserRouter(
   [
@@ -16,7 +35,81 @@ const router = createBrowserRouter(
     {
       path:'/rings',
       element: <Rings/>
+    },
+    {
+      path: '/About',
+      element: <About />
+    },
+    {
+      path: '/Contact',
+      element: <Contact />
+    },
+    {
+      path: '/Bracelets',
+      element: <Bracelets />
+    },
+    {
+      path: '/Earings',
+      element: <Earings />
+    },
+    {
+      path: '/Necklaces',
+      element: <Necklaces />
+    },
+    {
+      path: '/Productdetail',
+      element: <Productdetail />
+    },
+    {
+      path: '/Productdetail1',
+      element: <ProductDetail1 />
+    },
+    {
+      path: '/Productdetail2',
+      element: <ProductDetail2 />
+    },
+    {
+      path: '/Productdetail3',
+      element: <ProductDetail3 />
+    },
+    {
+      path: '/Productdetail4',
+      element: <ProductDetail4 />
+    },
+    {
+      path: '/Productdetail5',
+      element: <ProductDetail5 />
+    },
+    {
+      path: '/Productdetail6',
+      element: <ProductDetail6 />
+    },
+    {
+      path: '/Productdetail7',
+      element: <ProductDetail7 />
+    },
+    {
+      path: '/Productdetail8',
+      element: <ProductDetail8 />
+    },
+    {
+      path: '/Productdetail9',
+      element: <ProductDetail9 />
+    },
+    {
+      path: '/Productdetail10',
+      element: <ProductDetail10 />
+    },
+    {
+      path: '/Productdetail11',
+      element: <ProductDetail11 />
     }
+
+    
+
+   
+
+    
   ]
 )
 
@@ -26,5 +119,5 @@ createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
   <Footer/>
 
-  </StrictMode>,
+  </StrictMode>
 )
